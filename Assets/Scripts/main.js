87df4097e52c0f82500c48a80766b37b7b5ca61e
Bronson <bronson.schafer@gmail.com>
2022-11-01 21:39:02 -0600

    update main.js
    Added a function to regulate how many windows are open. The program will now block multiple of the same window being opened.

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -8,6 +8,9 @@ const {app, BrowserWindow, dialog, Menu} = electron;
 const fs = require('fs');
 
 let mainWindow;
+// keeps track of all open windows 
+// this will block the same window from being openned more than once
+const openWindows = [];
 
 // Listen for app to be ready 
 app.on('ready', function(){
@@ -32,6 +35,14 @@ app.on('ready', function(){
 
 // handle help page
 function openHelpMenu(){
+    // gives a name for tracking 
+    const winName = "help";
+
+    // checks if a window is already open 
+    if(isOpen(winName)){
+        return;
+    }
+
     // create new window 
     addWindow = new BrowserWindow({
         width: 400,
@@ -41,6 +52,27 @@ function openHelpMenu(){
     // load html into window 
     addWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'helpMenu.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+        removeWindow(winName);
+    });
+}
+
+// handle about page page
+function openAboutMenu(){
+    // create new window 
+    addWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        title: "About Page"
+    });
+    // load html into window 
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'aboutWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
@@ -76,7 +108,8 @@ const mainMenuTemplate = [
         click(){openHelpMenu();}
     },
     {
-        label: 'About'
+        label: 'About',
+        click(){ openAboutMenu();}
     },  
     {
         label: 'Quit', 
@@ -89,28 +122,6 @@ const mainMenuTemplate = [
 // if on mac, add empty object to menu
 if(process.platform == 'darwin'){
     mainMenuTemplate.unshift({});
-}
-
-// Add developer tools if not in production 
-if(process.env.NODE_ENV != 'productuion'){
-    mainMenuTemplate.push(
-        {
-        label: 'Developer Tools',
-        submenu:[
-            {
-                label: 'Toggle DevTools',
-                accelerator: process.platform == 'darwin' ? 'Commmand+I' : 'Ctrl+I',
-
-                click(item, focusedWindow){
-                    focusedWindow.toggleDevTools();
-                }
-            },
-            {
-                role: 'reload'
-            }
-        ]
-    }
-    );
 }
 
 // open file
@@ -130,4 +141,25 @@ function openFile(){
 
     const fileContent = fs.readdirSync(files.toString());
     console.log(fileContent);
+}
+
+// checks if a window is already open
+// adds it if not open 
+function isOpen(window){
+    if (openWindows.includes(window)){
+        return true;
+    }
+    // else
+    openWindows.push(window);
+    return false;
+}
+// removes open window when closed
+function removeWindow(window){
+    const index = openWindows.indexOf(window);
+    // checks that the index is valid
+    console.log(openWindows);
+    if (index > -1){
+        openWindows.splice(index, 1);
+    }
+    console.log(openWindows);
 }
